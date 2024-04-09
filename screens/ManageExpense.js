6b88d9217928c1dfabb1428448729eadd35e1157
Ruslan Lukoyanov <105169805+rLukoyanov@users.noreply.github.com>
2024-04-09 21:04:01 +0400

    Adding Custom Buttons

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,5 +1,8 @@
 import React, { useLayoutEffect } from "react";
-import { Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { IconButton } from "../components/UI/IconButton";
+import { GlobalStyles } from "../constants/styles";
+import { Button } from "../components/UI/Button";
 
 export const ManageExpense = ({ route, navigation }) => {
   const editedExpenseId = route.params?.expenseId;
@@ -11,5 +14,54 @@ export const ManageExpense = ({ route, navigation }) => {
     });
   }, [navigation, isEditing]);
 
-  return <Text>ManageExpenses</Text>;
+  function deleteExpenseHandler(params) {}
+  function cancelHandler(params) {}
+  function confitmHandler(params) {}
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.buttonsContainer}>
+        <Button style={styles.button} mode="flat" onPress={cancelHandler}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={confitmHandler}>
+          {isEditing ? "Update" : "Add"}
+        </Button>
+      </View>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
