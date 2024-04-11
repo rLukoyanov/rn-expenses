651caf083e74bcc25e709effa3cf686995b0b472
Ruslan Lukoyanov <105169805+rLukoyanov@users.noreply.github.com>
2024-04-11 18:36:07 +0400

    Handling User Input in a Generic Way

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,56 +1,74 @@
-import React from 'react';
-import {StyleSheet, Text, View} from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 import Input from "./Input";
 
 const ExpenseForm = () => {
-    function amountChangeHandler() {
+  const [inputValues, setInputValues] = useState({
+    amount: "",
+    date: "",
+    description: "",
+  });
 
-    }
+  function inputChangeHandler(inputId, enteredText) {
+    setInputValues((prevState) => ({ ...prevState, [inputId]: enteredText }));
+  }
 
-    return (
-        <View style={styles.form}>
-            <Text style={styles.title}>Your Expense</Text>
-            <View style={styles.inputsRow}>
-                <Input label="Amount" style={styles.rowInput} textInputConfig={{
-                    keyboardType: "decimal-pad",
-                    onChangeText: amountChangeHandler,
-                }}/>
-                <Input label="Date"
-                       style={styles.rowInput}
-                       textInputConfig={{
-                           placeholder: "YYYY-MM-DD",
-                           maxLength: 10,
-                           onChangeText: amountChangeHandler
-                       }}
-                />
-            </View>
+  return (
+    <View style={styles.form}>
+      <Text style={styles.title}>Your Expense</Text>
+      <View style={styles.inputsRow}>
+        <Input
+          label="Amount"
+          style={styles.rowInput}
+          textInputConfig={{
+            keyboardType: "decimal-pad",
+            onChangeText: inputChangeHandler.bind(null, "amount"),
+            value: inputValues.amount,
+          }}
+        />
+        <Input
+          label="Date"
+          style={styles.rowInput}
+          textInputConfig={{
+            placeholder: "YYYY-MM-DD",
+            maxLength: 10,
+            onChangeText: inputChangeHandler.bind(null, "date"),
+            value: inputValues.date,
+          }}
+        />
+      </View>
 
-            <Input label="Description" textInputConfig={{
-                multiline: true,
-                autocorrect: false,
-            }}/>
-        </View>
-    );
+      <Input
+        label="Description"
+        textInputConfig={{
+          multiline: true,
+          autocorrect: false,
+          onChangeText: inputChangeHandler.bind(null, "description"),
+          value: inputValues.description,
+        }}
+      />
+    </View>
+  );
 };
 
 export default ExpenseForm;
 
 const styles = StyleSheet.create({
-    form: {
-        marginTop: 80,
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: "bold",
-        color: '#fff',
-        marginVertical: 24,
-        textAlign: "center",
-    },
-    inputsRow: {
-        justifyContent: "space-between",
-        flexDirection: "row",
-    },
-    rowInput: {
-        flex: 1,
-    }
-})
+  form: {
+    marginTop: 80,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#fff",
+    marginVertical: 24,
+    textAlign: "center",
+  },
+  inputsRow: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+  },
+  rowInput: {
+    flex: 1,
+  },
+});
