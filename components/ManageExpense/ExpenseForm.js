4882c5c5df41081fc73270e353e7e0bc2000bdaa
Ruslan Lukoyanov <105169805+rLukoyanov@users.noreply.github.com>
2024-04-15 19:49:36 +0400

    Managing Form State & Submission

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Input from "./Input";
+import { Button } from "../UI/Button";
 
-const ExpenseForm = () => {
+const ExpenseForm = ({ onCancel, onSubmit, subbmitBtnLabel }) => {
   const [inputValues, setInputValues] = useState({
     amount: "",
     date: "",
@@ -12,6 +13,8 @@ const ExpenseForm = () => {
   function inputChangeHandler(inputId, enteredText) {
     setInputValues((prevState) => ({ ...prevState, [inputId]: enteredText }));
   }
+
+  function submitHandler(params) {}
 
   return (
     <View style={styles.form}>
@@ -47,6 +50,14 @@ const ExpenseForm = () => {
           value: inputValues.description,
         }}
       />
+      <View style={styles.buttonsContainer}>
+        <Button style={styles.button} mode="flat" onPress={onCancel}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={submitHandler}>
+          {subbmitBtnLabel}
+        </Button>
+      </View>
     </View>
   );
 };
@@ -70,5 +81,14 @@ const styles = StyleSheet.create({
   },
   rowInput: {
     flex: 1,
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,90 +1,77 @@
-import React, {useContext, useLayoutEffect} from "react";
-import {StyleSheet, View} from "react-native";
-import {IconButton} from "../components/UI/IconButton";
-import {GlobalStyles} from "../constants/styles";
-import {Button} from "../components/UI/Button";
-import {ExpensesContext} from "../store/expenses-context";
+import React, { useContext, useLayoutEffect } from "react";
+import { StyleSheet, View } from "react-native";
+import { IconButton } from "../components/UI/IconButton";
+import { GlobalStyles } from "../constants/styles";
+import { Button } from "../components/UI/Button";
+import { ExpensesContext } from "../store/expenses-context";
 import ExpenseForm from "../components/ManageExpense/ExpenseForm";
 
-export const ManageExpense = ({route, navigation}) => {
-    const {deleteExpense, updateExpense, addExpense} =
-        useContext(ExpensesContext);
-    const editedExpenseId = route.params?.expenseId;
-    const isEditing = !!editedExpenseId;
+export const ManageExpense = ({ route, navigation }) => {
+  const { deleteExpense, updateExpense, addExpense } =
+    useContext(ExpensesContext);
+  const editedExpenseId = route.params?.expenseId;
+  const isEditing = !!editedExpenseId;
 
-    useLayoutEffect(() => {
-        navigation.setOptions({
-            title: isEditing ? "Edit Expense" : "Add Expense",
-        });
-    }, [navigation, isEditing]);
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
 
-    function deleteExpenseHandler() {
-        deleteExpense(editedExpenseId);
-        navigation.goBack();
+  function deleteExpenseHandler() {
+    deleteExpense(editedExpenseId);
+    navigation.goBack();
+  }
+
+  function cancelHandler() {
+    navigation.goBack();
+  }
+
+  function confitmHandler() {
+    if (isEditing) {
+      updateExpense(editedExpenseId, {
+        description: "test",
+        amount: 99.99,
+        date: new Date(),
+      });
+    } else {
+      addExpense({ description: "test", amount: 99.99, date: new Date() });
     }
+    navigation.goBack();
+  }
 
-    function cancelHandler() {
-        navigation.goBack();
-    }
+  return (
+    <View style={styles.container}>
+      <ExpenseForm
+        onCancel={cancelHandler}
+        subbmitBtnLabel={isEditing ? "Update" : "Add"}
+      />
 
-    function confitmHandler() {
-        if (isEditing) {
-            updateExpense(editedExpenseId, {
-                description: "test",
-                amount: 99.99,
-                date: new Date(),
-            });
-        } else {
-            addExpense({description: "test", amount: 99.99, date: new Date()});
-        }
-        navigation.goBack();
-    }
-
-    return (
-        <View style={styles.container}>
-            <ExpenseForm/>
-            <View style={styles.buttonsContainer}>
-                <Button style={styles.button} mode="flat" onPress={cancelHandler}>
-                    Cancel
-                </Button>
-                <Button style={styles.button} onPress={confitmHandler}>
-                    {isEditing ? "Update" : "Add"}
-                </Button>
-            </View>
-            {isEditing && (
-                <View style={styles.deleteContainer}>
-                    <IconButton
-                        icon="trash"
-                        color={GlobalStyles.colors.error500}
-                        size={36}
-                        onPress={deleteExpenseHandler}
-                    />
-                </View>
-            )}
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteExpenseHandler}
+          />
         </View>
-    );
+      )}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 24,
-        backgroundColor: GlobalStyles.colors.primary800,
-    },
-    deleteContainer: {
-        marginTop: 16,
-        paddingTop: 8,
-        borderTopWidth: 2,
-        borderTopColor: GlobalStyles.colors.primary200,
-        alignItems: "center",
-    },
-    button: {
-        minWidth: 120,
-        marginHorizontal: 8,
-    },
-    buttonsContainer: {
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-    },
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  },
 });
